fde41559f0089cf8c6a24edc1fd7d7b970849507
Jorge David <jorge.fernandez@ukg.com>
2022-12-07 13:27:48 -0500

    delete obsolete

--- Display Gadget Messages/app_code/FeatureFlags/DashboardHubCICDFeature.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using UltimateSoftware.Security;
-
-public static class DashboardHubCICDFeature
-{
-    public static FeatureFlag ForClient(string clientID){
-        return new FeatureFlag(clientID, "Wayfinding", "SmartDashboardHubCICDEnabled");
-    }
-}
--- Display Gadget Messages/app_code/FeatureFlags/UnifiedNavigationMfeFeature.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using UltimateSoftware.Security;
-
-public static class UnifiedNavigationMfeFeature
-{
-    public static FeatureFlag ForClient(string clientID){
-        return new FeatureFlag(clientID, "Wayfinding", "UnifiedNavigationMfeFeatureEnabled");
-    }
-}
